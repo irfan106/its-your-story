4fccdfb44a7c05f14421e4d6329c072a696fb264
irfan <ir.irfan106@gmail.com>
2022-05-31 08:23:46 +0530

    Its Your Story, So Tell Everyone

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+        <div>Its your Story, So Tell Everyone!!!</div>
+    </footer>
+  )
+}
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwpUekvnYsr5qdj2BZPQs_teuukcKoV50',
+  authDomain: 'its-your-story.firebaseapp.com',
+  projectId: 'its-your-story',
+  storageBucket: 'its-your-story.appspot.com',
+  messagingSenderId: '905651157300',
+  appId: '1:905651157300:web:10d9d77f6a6a65874f8d61',
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
